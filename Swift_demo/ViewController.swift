a52918a35b3c90d45fe92f63612608d3089bdef4
sangle-umesh <umesh.sangle@ninedots.com>
2016-03-31 11:54:05 +0530

    c1

--- Swift_demo/ViewController.swift
+++ Swift_demo/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("this wat!")
         // Dispose of any resources that can be recreated.
     }
 
